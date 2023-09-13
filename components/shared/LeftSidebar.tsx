065a5bffc8b039ed6f1e3a5e1b8ff024396906b6
hmoa01 <hmoa01@gmail.com>
2023-09-13 18:51:30 +0200

    add left sidebar layout

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,4 +1,57 @@
+"use client";
+
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+
+import Image from "next/image";
+import Link from "next/link";
+import { sidebarLinks } from "@/constants";
+
 function LeftSidebar() {
-  return <h1>LeftSidebar</h1>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              className={`leftsidebar_link ${isActive && "bg-primary-500"} `}
+              href={link.route}
+              key={link.label}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
 }
 export default LeftSidebar;
