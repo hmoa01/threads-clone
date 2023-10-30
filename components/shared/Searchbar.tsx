b1ab4848a84bf2e53a1dd0aeaa9f7350ae175db8
hmoa01 <hmoa01@gmail.com>
2023-10-30 18:35:19 +0100

    add search

---
+++ components/shared/Searchbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+import { Input } from "../ui/input";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  routeType: string;
+}
+
+const Searchbar = ({ routeType }: Props) => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        router.push(`/${routeType}?q=` + search);
+      } else {
+        router.push(`/${routeType}`);
+      }
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, routeType]);
+  return (
+    <div className="searchbar">
+      <Image
+        src="/assets/search-gray.svg"
+        alt="search"
+        width={24}
+        height={24}
+        className="object-contain"
+      />
+      <Input
+        id="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder={`${
+          routeType !== "/search" ? "Search communities" : "Search creators"
+        }`}
+        className="no-focus searchbar_input"
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,11 @@
 "use server";
 
+import { SortOrder, _FilterQuery } from "mongoose";
+
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
+import { getJsPageSizeInKb } from "next/dist/build/utils";
 import path from "path";
 import { revalidatePath } from "next/cache";
 
@@ -77,5 +80,55 @@ export async function fetchUserPosts(userId: string) {
     return threads;
   } catch (error: any) {
     throw new Error(`Failed to fetch user posts: ${error.message}`);
+  }
+}
+
+export async function fetchUsers({
+  userId,
+  searchString = "",
+  pageNumber = 1,
+  pageSize = 20,
+  sortBy = "desc",
+}: {
+  userId: string;
+  searchString?: string;
+  pageNumber?: number;
+  pageSize?: number;
+  sortBy?: SortOrder;
+}) {
+  try {
+    connectToDB();
+
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const regex = new RegExp(searchString, "i");
+
+    const query: _FilterQuery<typeof User> = {
+      id: { $ne: userId },
+    };
+
+    if (searchString.trim() !== "") {
+      query.$or = [
+        { username: { $regex: regex } },
+        { name: { $regex: regex } },
+      ];
+    }
+
+    const sortOptions = { createdAt: sortBy };
+
+    const usersQuery = User.find(query)
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalUsersCount = await User.countDocuments(query);
+
+    const users = await usersQuery.exec();
+
+    const isNext = totalUsersCount > skipAmount + User.length;
+
+    return { users, isNext };
+  } catch (error: any) {
+    throw new Error(`Failed to fetch users: ${error.message}`);
   }
 }
